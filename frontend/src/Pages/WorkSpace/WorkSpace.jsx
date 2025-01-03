36d519f3005081de949a0d37fc29a38dba8192b4
livan116 <livankumar112@gmail.com>
2025-01-03 22:44:53 +0530

    frontend2

--- frontend/src/Pages/WorkSpace/WorkSpace.jsx
+++ frontend/src/Pages/WorkSpace/WorkSpace.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import style from "./workSpace.module.css";
+import style from "./WorkSpace.module.css";
 import { useNavigate } from "react-router-dom";
 import NavHead from "../NavHead/NavHead";
 import { useTheme } from "../../Context/ThemeContext";
